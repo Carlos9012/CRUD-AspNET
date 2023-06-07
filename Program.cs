aa28d66347f20c6bf1f43bda253436eda7ecf757
Carlos9012 <francisco.carlos1564@gmail.com>
2023-06-07 10:00:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using Cards.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CardsDbContext>(opitions =>
+opitions.UseSqlServer(builder.Configuration.GetConnectionString("CardsDbConnectionString")));
+
+
+
+
+builder.Services.AddCors((setup) =>
+{
+    setup.AddPolicy("default", (options) =>
+    {
+        options.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+        options.WithOrigins("http://localhost:4200");
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("default");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
